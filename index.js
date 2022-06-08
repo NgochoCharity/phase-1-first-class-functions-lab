e6f02b1aaf1f12f0127bc82fe45b78ad3799ec60
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-08 13:05:52 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = ()  => {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+   return (drivers.slice(0, 2))
+}
+returnFirstTwoDrivers ()
+ 
+const returnLastTwoDrivers = () => {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return (drivers.slice(-2))
+}
+returnLastTwoDrivers()
+
+const selectingDrivers = [  returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = fare =>
+    function fareMultiplier (multiplier) {  
+        return fare * multiplier
+    }
+
+ function fareDoubler(fare)  {
+     return fare*2
+ }
+
+function fareTripler (fare) {
+    return fare * 3}
+
+
+function selectDifferentDrivers (drivers, myFunc){
+return myFunc(drivers);
+
+}
+    
+
+
+
+
+
