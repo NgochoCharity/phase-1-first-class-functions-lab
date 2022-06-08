b84ccd307efc0718c0b3e4d1a612269d573bd3fe
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-08 11:26:04 +0300

    commit tree 8109d13e3f355ada3ec238e4eedc8b509c6f1d36

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = ()  => {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+   return (drivers.slice(0, 2))
+}
+returnFirstTwoDrivers ()
+ 
+const returnLastTwoDrivers = () => {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return (drivers.slice(-2))
+}
+returnLastTwoDrivers()
+
+const selectingDrivers = [  returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fare)  => {
+const fareMultiplier = () => createFareMultiplier (2)
+return fareMultiplier*m
+}
